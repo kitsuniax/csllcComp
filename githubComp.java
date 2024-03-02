d538cd32e1339c39a774fcdb9ef7c86458771124
Audrey Shin <ajs@nbp-203-49.nbp.ruw.rutgers.edu>
2024-03-01 19:37:41 -0500

    Audrey added a line of code

--- githubComp.java
+++ githubComp.java
@@ -3,8 +3,9 @@ public class githubComp {
         // ROUND 1
         String str = "abcdefghijklmnopqrstuvwxyz"; // editor: Iris
                                                    // note: made string for round 1
-        for (int i = 0; i < str.length(); i++) {
-
+        for (int i = 0; i < str.length(); i++) { //editor: Pallavi
+            if (i%3 == 0 && i%2 ==1){ //editor: Audrey
+            }
         }
 
     }
