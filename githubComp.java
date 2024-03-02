fce9afdfb8109e9c4082688a91d977008017281f
kitsuniax <146673662+kitsuniax@users.noreply.github.com>
2024-03-01 19:40:59 -0500

    round1edit3Iris
    added print statement to print out values that fulfill the conditions of the if statement.

--- githubComp.java
+++ githubComp.java
@@ -5,6 +5,7 @@ public class githubComp {
                                                    // note: made string for round 1
         for (int i = 0; i < str.length(); i++) { //editor: Pallavi
             if (i%3 == 0 && i%2 ==1){ //editor: Audrey
+                System.out.println(str.charAt(i));  //editor: Iris
             }
         }
 
