596ce4dd8c86735d93542f630586612e04ea6a5e
Pallavi <96259165+pallavipan99@users.noreply.github.com>
2024-03-01 19:32:17 -0500

    Pallavi
    "Used a for loop to iterate through the string  for round 1 edit 2."
    
    Co-Authored-By: ajs787 <152229159+ajs787@users.noreply.github.com>
    Co-Authored-By: kitsuniax <146673662+kitsuniax@users.noreply.github.com>

--- githubComp.java
+++ githubComp.java
@@ -1,8 +1,11 @@
-public class githubComp{
-    public static void main (String[] args){
-        //ROUND 1
-        String str = "abcdefghijklmnopqrstuvwxyz";  //editor: Iris
-                                                    //note: made string for round 1
+public class githubComp {
+    public static void main(String[] args) {
+        // ROUND 1
+        String str = "abcdefghijklmnopqrstuvwxyz"; // editor: Iris
+                                                   // note: made string for round 1
+        for (int i = 0; i < str.length(); i++) {
+
+        }
 
     }
 }
