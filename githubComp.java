c38ee777a2b2f7839f006e64cc96490c28abc4b0
Pallavi <96259165+pallavipan99@users.noreply.github.com>
2024-03-01 19:45:20 -0500

    PallaviRound2Edit1
    Made an array and a variable
    
    Co-Authored-By: ajs787 <152229159+ajs787@users.noreply.github.com>
    Co-Authored-By: kitsuniax <146673662+kitsuniax@users.noreply.github.com>

--- githubComp.java
+++ githubComp.java
@@ -3,11 +3,17 @@ public class githubComp {
         // ROUND 1
         String str = "abcdefghijklmnopqrstuvwxyz"; // editor: Iris
                                                    // note: made string for round 1
-        for (int i = 0; i < str.length(); i++) { //editor: Pallavi
-            if (i%3 == 0 && i%2 ==1){ //editor: Audrey
-                System.out.println(str.charAt(i));  //editor: Iris
+        for (int i = 0; i < str.length(); i++) { // editor: Pallavi
+            if (i % 3 == 0 && i % 2 == 1) { // editor: Audrey
+                System.out.println(str.charAt(i)); // editor: Iris
             }
         }
+        // ROUND 2
+        int[][] matrix = { { 1, 3, 1, 4 }, // created matrix
+                { 0, 2, 1, 1 },
+                { 0, 0, 2, 3 },
+                { 0, 0, 0, 1 } };
+        int product = 1;
 
     }
 }
