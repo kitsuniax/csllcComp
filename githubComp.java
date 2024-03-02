ef8efe2e02bb04eea0b40c0b61477e06325fd539
kitsuniax <146673662+kitsuniax@users.noreply.github.com>
2024-03-01 19:16:38 -0500

    round1edit1Iris
    added 1 line code: initialized string variable a-z

---
+++ githubComp.java
@@ -0,0 +1,8 @@
+public class githubComp{
+    public static void main (String[] args){
+        //ROUND 1
+        String str = "abcdefghijklmnopqrstuvwxyz";  //editor: Iris
+                                                    //note: made string for round 1
+
+    }
+}
